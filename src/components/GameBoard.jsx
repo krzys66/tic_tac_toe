07487ed6f8ee541cc963dffc0b52963b2256d74d
Your Name <you@example.com>
2025-03-24 11:02:42 +0100

    add game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+const board = [
+    ["O", null, null],
+    [null, "X", null],
+    [null, null, null]
+]
+
 export default function GameBoard() {
   return (
-    <div>GameBoard</div>
+    <ul id='game-board'>
+        {board.map((row, rowIndex) => {
+            return (
+            <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex) => {
+                        return (
+                            <li key={colIndex}>
+                                <button>{playerSymbol}</button>
+                            </li>
+                        )
+                    })}
+                </ol>
+            </li>
+            )
+        })}
+    </ul>
   )
 }
